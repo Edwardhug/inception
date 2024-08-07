3d696a9df491d1c62f41d48f80c9021e3da74bd9
Mac de Lucien <luciengabet0@gmail.com>
2024-08-07 14:23:46 +0200

    new error with wordpress which cant connect to mariadb

--- srcs/requirements/mariadb/conf/init-db.sh
+++ srcs/requirements/mariadb/conf/init-db.sh
@@ -2,6 +2,10 @@
 
 service mysql start
 
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
+
 mysql -u root <<-EOSQL
     CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};
     CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_PASSWORD}';
